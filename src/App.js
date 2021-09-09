b69f8dc50e9ba7031a1a1c591a57a0820bb8dbc5
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2021-09-09 22:00:48 +0300

    React ADD to cart ../REMOWE THE LIST//ADD THE LIST/REMOWE THE ITEM/ ADD TO ITEM// REACT CO=USOM HOOKS/ REACT ROUTER

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Final from './tutorial/12-memo-useMemo-useCallback/final/index'
+
+function App() {
+  return (
+    <div className='container'>
+    
+      <Final/>
+      
+      
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const data = [
+  { id: 1, name: 'parvin' },
+  { id: 2, name: 'Farid' },
+  { id: 3, name: 'Mamed' },
+  { id: 5, name: 'Eli' },
+  { id: 6, name: 'Ramin' },
+  { id: 7, name: 'Elixan' },
+  { id: 8, name: 'Tofiq' },
+  { id: 9, name: 'Yusif' },
+  { id: 10, name: 'Front Developer' },
+  { id:11, name: 'Front Developer' },
+  { id: 12, name: 'Front Developer' },
+  { id: 13, name: 'Front Developer' },
+  { id: 14, name: 'Front Developer' },
+ 
+];
